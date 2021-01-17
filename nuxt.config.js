b5c12417c11ce4d934e90d383db0664d850b8bbb
周锋 <zfeng16@grgbanking.com>
2021-01-17 12:51:35 +0800

    ci: 自动化测试

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: '流云辞-6',
+    title: '流云辞-7',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
