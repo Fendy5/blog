371e1943f02eb424cd29d98795fa58cffeb43897
Fendy <zf@fendy5.cn>
2021-08-10 10:45:14 +0800

    🎉Initial commit~

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <fd-header />
+    <Nuxt />
+    <fd-footer />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import FdHeader from '@/layouts/FdHeader.vue'
+import FdFooter from '@/layouts/FdFooter.vue'
+
+export default Vue.extend({
+  name: 'Layout',
+  components: {
+    FdHeader,
+    FdFooter
+  }
+})
+</script>
+
+<style lang="scss">
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,100 @@
+import { resolve } from 'path'
+
+export default {
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: '流云辞',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    '@/assets/scss/_common.scss'
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+    '@/plugins/element-ui',
+    '@/plugins/svg-icon'
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss'
+  ],
+
+  server: {
+    port: 6080, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios'
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {
+    proxy: true,
+    prefix: '/dev-api',
+    credentials: true
+  },
+
+  proxy: {
+    '/dev-api': {
+      target: 'http://127.0.0.1:7036',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/dev-api': ''
+      }
+    }
+  },
+
+  env: {
+    BASE_URL: process.env.BASE_URL
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+    extend (config) {
+      // 排除 nuxt 原配置的影响,Nuxt 默认有vue-loader,会处理svg,img等
+      // 找到匹配.svg的规则,然后将存放svg文件的目录排除
+      const svgRule = config.module.rules.find(rule => rule.test.test('.svg'))
+      svgRule.exclude = [resolve(__dirname, 'assets/icons/svg')]
+
+      // 添加loader规则
+      config.module.rules.push({
+        test: /\.svg$/, // 匹配.svg
+        include: [resolve(__dirname, 'assets/icons/svg')], // 将存放svg的目录加入到loader处理目录
+        use: [{
+          loader: 'svg-sprite-loader',
+          options: { symbolId: 'icon-[name]' }
+        }]
+      })
+    }
+  },
+  babel: {
+    plugins: [
+      ['component',
+        {
+          libraryName: 'element-ui',
+          styleLibraryName: 'theme-chalk'
+        }
+      ]
+    ]
+  }
+}
