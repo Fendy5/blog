bdc3522af13d055295b4073cdb05aa344f269ca4
Fendy <zf@fendy5.cn>
2021-08-10 20:46:35 +0800

    ♻️build: change nuxt.config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -105,8 +105,11 @@ export default {
       }
     }
   },
+  generate: {
+    fallback: 'index.html'
+  },
   router: {
-    mode: 'hash', // 使用 'hash' 主要是为了适配以相对路径打开的静态站点， 必须使用 'hash' 否则路由跳转不生效
+    // mode: 'hash', // 使用 'hash' 主要是为了适配以相对路径打开的静态站点， 必须使用 'hash' 否则路由跳转不生效
     base: process.env.NODE_ENV === 'production' ? './' : '/' // 使用 './' 主要是为了适配以相对路径打开的静态站点
   }
 }
