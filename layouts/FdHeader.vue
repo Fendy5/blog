37032f93a765c386d4af9dcb6c76428fcebccd88
Fendy <zf@fendy5.cn>
2021-09-06 16:13:52 +0800

    ✨feat: perfect animation of menu button

--- layouts/FdHeader.vue
+++ layouts/FdHeader.vue
@@ -2,8 +2,8 @@
   <header class="flex font-bold">
     <div class="fd-container z-10 flex-justify-between items-center flex">
       <nuxt-link class="flex-items-center" to="/">
-        <svg-icon class="wh-3rem" icon-class="logo" />
-        <span class="text-xl">流云辞</span>
+        <svg-icon class="wh-25" icon-class="logo" />
+        <span class="text-xl pl-1">流云辞</span>
       </nuxt-link>
       <ul class="nav">
         <li v-for="(i,index) in menuList" :key="index">
@@ -144,7 +144,7 @@ header{
         &.active {
           .line1 {
             transform: rotate(125deg);
-            top: 3px;
+            top: 4px;
             position: relative;
           }
 
