60b71eb53992fa1d8e0398909bfe7c7d15803738
Fendy <zf@fendy5.cn>
2021-09-16 11:13:28 +0800

    ✨feat: add beian link

--- layouts/FdFooter.vue
+++ layouts/FdFooter.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="text-gray-600 fd-container my-4 text-center">
-    <p>粤ICP备17077927号-1</p>
+    <p>
+      <a href="https://beian.miit.gov.cn/">粤ICP备17077927号-1</a>
+    </p>
     <p>Copyright ©{{ getYear }} All rights reserved | This website is made  by <a class="text-primary" href="https://fendy5.cn">Fendy</a></p>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
   computed: {
