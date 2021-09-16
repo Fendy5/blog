77c4810c3b2276155f1fa727110df7f41b811cb0
Fendy <zf@fendy5.cn>
2021-09-16 19:50:59 +0800

    ✨feat: ArticlePagination Component

---
+++ components/ArticlePagination.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="text-right mt-12 mb-16">
+    <el-pagination
+      :hide-on-single-page="true"
+      :current-page="page.current_page"
+      :page-sizes="[6,12,30,50]"
+      :page-size="page.per_page"
+      layout="total, prev, pager, next"
+      :total="page.total"
+      @current-change="handleCurrentChange"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { getArticleListApi } from '~/api/article'
+
+export default Vue.extend({
+  props: {
+    tempPage: {
+      type: Object,
+      default: () => { return [] }
+    }
+  },
+  data () {
+    return {
+      page: {
+        total: 0,
+        per_page: 6,
+        current_page: 1
+      }
+    }
+  },
+  created () {
+    Object.keys(this.page).forEach((key) => { this.page[key] = this.tempPage[key] })
+  },
+  methods: {
+    handleCurrentChange (val: number) {
+      this.page.current_page = val
+      this.$router.push({ query: { per_page: this.page.per_page, current_page: val }, path: `${this.$route.fullPath}` })
+      this.getList()
+    },
+    getList (page: number = this.page.current_page, rowsPerPage: number = this.page.per_page) {
+      getArticleListApi({ page, rowsPerPage }).then((value) => {
+        const data = value.data
+        this.$emit('pageChanged', data.data)
+        Object.keys(this.page).forEach((key) => { this.page[key] = parseInt(data[key]) })
+      })
+    }
+  }
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
