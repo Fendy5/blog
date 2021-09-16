e4647868df049657e240ce8bd0c12f8c0701befa
Fendy <zf@fendy5.cn>
2021-09-16 17:57:05 +0800

    ✨feat: ArticleList Component

---
+++ components/ArticleList.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="blog-left-sidebar">
+    <div v-if="articles.length" class="blog-list">
+      <div v-for="i in articles" :key="i.id" class="blog-item">
+        <div class="blog-cover">
+          <cover :url="i.cover" alt="图片暂无法显示" />
+          <div class="blog-date">
+            <h3>10日</h3>
+            <p>07月</p>
+          </div>
+        </div>
+        <div class="blog-detail">
+          <nuxt-link :title="i.title" :to="`/s/${i.article_id}`">
+            <h2>{{ i.title }}</h2>
+          </nuxt-link>
+          <p>{{ i.summary }}</p>
+          <ul class="divide-x">
+            <li class="flex-items-center pr-4">
+              <svg-icon class="wh-25 mr-1" icon-class="sort" />
+              <NuxtLink to="/">算法</NuxtLink>
+            </li>
+            <li class="flex-items-center pl-4">
+              <svg-icon class="wh-25 mr-1" icon-class="comment" />
+              <NuxtLink to="/">评论(0)</NuxtLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div v-else class="text-center">
+      <svg-icon class="empty-icon" icon-class="empty" />
+      <p class="text-secondary mt-12">暂无内容</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { articleMixin } from '~/mixins/article'
+import Cover from '~/components/Cover.vue'
+import { Article } from '~/types'
+
+export default Vue.extend({
+  name: 'ArticleList',
+  components: { Cover },
+  mixins: [articleMixin],
+  props: {
+    list: {
+      type: Array,
+      default: () => { return [] }
+    }
+  },
+  computed: {
+    articles (): Article[] {
+      return this.list.map((value: any) => {
+        const time = value.updated_at.split(' ')[0].split('-')
+        value.month = time[1]
+        value.day = time[2]
+        return value
+      })
+    }
+  }
+})
+
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/index";
+@media #{$desktop} {
+  .blog-list {
+    @apply grid grid-cols-2 gap-x-4;
+  }
+}
+</style>
