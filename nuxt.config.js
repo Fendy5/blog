62c6bb3ae94044a1a5ea5515ae0d6305884241af
Fendy <zf@fendy5.cn>
2021-09-26 19:36:59 +0800

    ✨feat: add robots

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,8 +45,18 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/robots',
+    {
+      UserAgent: '*',
+      Allow: '*'
+    }
   ],
+
+  robots: {
+    UserAgent: '*',
+    Allow: '*'
+  },
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
