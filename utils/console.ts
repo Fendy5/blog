5353b72ecfac316e5581b57a5ed54d87ad65219a
Fendy <zf@fendy5.cn>
2021-10-04 00:39:48 +0800

    🔇chore: add log

--- utils/console.ts
+++ utils/console.ts
@@ -10,6 +10,7 @@ console.log(`
                                                        /     /'
                                                       (___,/'
   `)
+console.log('\\{^_^}/ hi!')
 console.log('%c欢迎光临Fendy的个人博客', 'color: #3b13b0; font-style: italic')
 console.log('%c本网站是基于Nuxt SSR渲染+Laravel的个人博客', 'color: #409EFF;')
 console.log('%c本站源码地址为 https://github.com/Fendy5/blog', 'color: #E6A23C; font-style: italic')
